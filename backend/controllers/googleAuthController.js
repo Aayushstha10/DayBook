f4926fe91bed5bb72f464d76b74ce39bf0282125
Aayush Shrestha <119415261+Aayushstha10@users.noreply.github.com>
2026-08-04 21:28:50 +0545

    update mail

--- backend/controllers/googleAuthController.js
+++ backend/controllers/googleAuthController.js
@@ -17,7 +17,7 @@ exports.googleLogin = async (req, res) => {
 
     const { email, name, picture, sub: googleId } = payload;
 
-    const ADMIN_EMAIL = "aayushshrestha003@gmail.com" ||"barsha@gmail.com";
+    const ADMIN_EMAILS = ["aayushshrestha003@gmail.com", "barsha@gmail.com"];
 
     let user = await User.findOne({ email });
 
@@ -29,9 +29,7 @@ exports.googleLogin = async (req, res) => {
         googleId,
         authProvider: "google",
         role:
-          email.toLowerCase() === ADMIN_EMAIL.toLowerCase()
-            ? "admin"
-            : "user",
+          email.toLowerCase() === ADMIN_EMAIL.toLowerCase() ? "admin" : "user",
       });
     } else {
       if (!user.googleId) {
@@ -56,7 +54,7 @@ exports.googleLogin = async (req, res) => {
       process.env.secret,
       {
         expiresIn: "7d",
-      }
+      },
     );
 
     res.status(200).json({
--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,35 +1,46 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+const ADMIN_EMAILS = ["aayushshrestha003@gmail.com", "barsha@gmail.com"];
+
 const signup = async (req, res) => {
   try {
     const { email, name, password } = req.body;
+
     const isexist = await User.findOne({ email });
+
     if (isexist) {
-      return res.status(400).json({ message: "user already exist" });
+      return res.status(400).json({
+        message: "User already exists",
+      });
     }
 
-    const hashpasword = await bcrypt.hash(password, 10);
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const isAdmin = ADMIN_EMAILS.includes(email.toLowerCase());
+
     const user = await User.create({
       name,
       email,
-      password: hashpasword,
+      password: hashPassword,
+      role: isAdmin ? "admin" : "user",
     });
 
     res.status(201).json({
-      message: "user create successfuly",
+      message: "User created successfully",
       user: {
         id: user._id,
         name: user.name,
         email: user.email,
+        role: user.role,
       },
     });
   } catch (err) {
     console.log(err);
     res.status(500).json({
-      message: "server error",
+      message: "Server error",
     });
   }
 };
 
-module.exports=signup;
+module.exports = signup;
